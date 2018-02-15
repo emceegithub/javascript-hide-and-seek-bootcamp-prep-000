01aa2bc76bf97ae252baffc5697f3ad0cc2cc65c
Mark Cruz <markchriscruz@yahoo.com>
2018-02-15 17:51:44 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector('div.target')
+}
+
+function deepestChild() {
+  return document.querySelector('div#grand-node div div div div')
+}
+
+function increaseRankBy(n) {
+  var collector = document.querySelectorAll("ul.ranked-list li")
+  for (var i=0;i<collector.length;i++) {
+    collector[i].innerHTML = parseInt(collector[i].innerHTML) + n
+  }
+}
